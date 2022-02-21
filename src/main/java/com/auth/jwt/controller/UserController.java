54802beec7f8062221c6c210383b7817f49b35b9
Rainhardvidiansyah <rainhardvidians@gmail.com>
2022-02-22 00:07:38 +0700

    add saving role functionality

--- src/main/java/com/auth/jwt/controller/UserController.java
+++ src/main/java/com/auth/jwt/controller/UserController.java
@@ -6,6 +6,7 @@ import com.auth.jwt.user.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,6 +41,15 @@ public class UserController {
     @GetMapping("/getappuser")
     public List<Appuser> app(){
         return appusers;
+    }
+
+    @PostMapping("/save/user")
+    public ResponseEntity<Appuser>  saveUser(Appuser appuser){
+        return ResponseEntity.ok().body(userService.saveUser(appuser));
+    }
+
+    public ResponseEntity<Role> saveRole(Role role){
+        return ResponseEntity.ok().body(userService.saveRole(role));
     }
 
 
