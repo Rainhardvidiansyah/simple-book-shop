30b38a5d484c9e366731f7ec7f34fe89402056fb
Rainhardvidiansyah <rainhardvidians@gmail.com>
2022-08-12 19:24:39 +0700

    File to response a request book via author

---
+++ src/main/java/com/auth/jwt/dto/response/FindAuthorAndBooksResponse.java
@@ -0,0 +1,46 @@
+package com.auth.jwt.dto.response;
+
+
+import com.auth.jwt.model.Book;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+public class FindAuthorAndBooksResponse {
+
+    private String bookTitle;
+    private String author;
+    private String synopsis;
+    private int pages;
+    private String price;
+    private String paperType;
+    private int stocks;
+    private String isbn;
+    private String dateOfPublished;
+
+    public FindAuthorAndBooksResponse(String bookTitle, String author, String synopsis, int pages,
+                                      String price, String paperType, int stocks, String isbn,
+                                      String dateOfPublished) {
+        this.bookTitle = bookTitle;
+        this.author = author;
+        this.synopsis = synopsis;
+        this.pages = pages;
+        this.price = price;
+        this.paperType = paperType;
+        this.stocks = stocks;
+        this.isbn = isbn;
+        this.dateOfPublished = dateOfPublished;
+    }
+
+    public FindAuthorAndBooksResponse From(Book book){
+//        new FindAuthorAndBooksResponse(
+//                books.getTitle(), books.getAuthor(), books.getSynopsis(), books.getPages(),
+//                books.getPrice(), books.getPaperType(), books.getStocks(),
+//                books.getIsbn(), books.getDateOfPublished());
+        FindAuthorAndBooksResponse response = new FindAuthorAndBooksResponse();
+        response.setBookTitle(book.getTitle());
+        response.setAuthor(book.getAuthor());
+        return response;
+    }
+}
