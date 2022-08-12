a1cc0a084adc88e965f58f119714d73054ebf7c8
Rainhardvidiansyah <rainhardvidians@gmail.com>
2022-08-12 21:20:17 +0700

    Add Book Response to http response

---
+++ src/main/java/com/auth/jwt/dto/response/BookResponse.java
@@ -0,0 +1,45 @@
+package com.auth.jwt.dto.response;
+
+import com.auth.jwt.model.Book;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Optional;
+
+@NoArgsConstructor @Getter @Setter
+public class BookResponse {
+
+    private String bookTitle;
+    private String author;
+    private String synopsis;
+    private int pages;
+    private String price;
+    private String paperType;
+    private int stocks;
+    private String isbn;
+    private String dateOfPublished;
+    private String dateOfUpload;
+
+    public BookResponse(String bookTitle, String author, String synopsis, int pages,
+                        String price, String paperType, int stocks, String isbn,
+                        String dateOfPublished, String dateOfUpload) {
+        this.bookTitle = bookTitle;
+        this.author = author;
+        this.synopsis = synopsis;
+        this.pages = pages;
+        this.price = price;
+        this.paperType = paperType;
+        this.stocks = stocks;
+        this.isbn = isbn;
+        this.dateOfPublished = dateOfPublished;
+        this.dateOfUpload = dateOfUpload;
+    }
+
+    public static BookResponse From(Book book) {
+        return new BookResponse(
+                book.getTitle(), book.getAuthor(), book.getSynopsis(), book.getPages(),
+                book.getPrice(), book.getPaperType(), book.getStocks(),
+                book.getIsbn(), book.getDateOfPublished(), book.getDateOfUpload());
+    }
+}
