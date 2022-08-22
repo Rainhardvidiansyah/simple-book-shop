0440efd6039ab4f1b2a293bcd033a7a5a01ab12a
Rainhardvidiansyah <rainhardvidians@gmail.com>
2022-08-22 14:30:32 +0700

    add image field in favorite response

--- src/main/java/com/auth/jwt/dto/response/FavoriteResponseInJoin.java
+++ src/main/java/com/auth/jwt/dto/response/FavoriteResponseInJoin.java
@@ -1,7 +1,10 @@
 package com.auth.jwt.dto.response;
 
+import com.auth.jwt.model.BookImage;
 import com.auth.jwt.model.Favorite;
 import lombok.*;
+
+import java.util.List;
 
 @Getter @Setter @ToString @NoArgsConstructor
 public class FavoriteResponseInJoin {
@@ -12,15 +15,19 @@ public class FavoriteResponseInJoin {
 
     private String author;
 
-    public FavoriteResponseInJoin(Long bookId, String title, String author) {
+    private List<BookImage> picture;
+
+
+    public FavoriteResponseInJoin(Long bookId, String title, String author, List <BookImage> bookImages) {
         this.bookId = bookId;
         this.title = title;
         this.author = author;
+        this.picture = bookImages;
     }
 
     public static FavoriteResponseInJoin From(Favorite favorite){
         return new FavoriteResponseInJoin(favorite.getBook().getId(), favorite.getBook().getTitle(),
-                favorite.getBook().getAuthor());
+                favorite.getBook().getAuthor(), favorite.getBook().getBookImage());
     }
 
 }
