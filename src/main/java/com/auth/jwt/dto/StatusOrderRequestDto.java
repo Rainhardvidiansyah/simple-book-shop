5d272c8f2f9c153820802f7828e53dabb0285a01
Rainhardvidiansyah <rainhardvidians@gmail.com>
2022-09-02 20:53:57 +0700

    Make status order dto

---
+++ src/main/java/com/auth/jwt/dto/StatusOrderRequestDto.java
@@ -0,0 +1,13 @@
+package com.auth.jwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor @AllArgsConstructor
+@Getter @Setter
+public class StatusOrderRequestDto {
+
+    private boolean status_order;
+}
