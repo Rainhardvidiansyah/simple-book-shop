6a385a962137eb45b1ad7a67bd609b35e90aba3c
Rainhardvidiansyah <rainhardvidians@gmail.com>
2022-09-22 16:56:50 +0700

    Fixing and giving logging to Favorite Service and Favorite Controller

--- src/main/java/com/auth/jwt/controller/FavoriteController.java
+++ src/main/java/com/auth/jwt/controller/FavoriteController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/favorite")
@@ -27,34 +28,42 @@ public class FavoriteController {
 
     private final FavoriteService favoriteService;
     private final UserRepo userRepo;
-    private final BooksRepo booksRepo;
     private final HttpServletRequest servletRequest;
 
     @PostMapping("/add")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
     public ResponseEntity<?> makeFavorite(@RequestBody FavoriteRequestDto favRequestDto){
         var email = servletRequest.getUserPrincipal().getName();
         AppUser user = userRepo.findAppUserByEmail(email)
                 .orElseThrow(RuntimeException::new);
-        var savedFavorite = favoriteService.createFavorite(favRequestDto.getBookId(), user);
+        var savedFavorite = favoriteService.createFavorite(favRequestDto.getBookId(),
+                favRequestDto.getContent(), user);
         log.info("Book saved as favorite: {}", savedFavorite.getBook().getTitle());
         return new ResponseEntity<>(new FavoriteResponse(savedFavorite.getContent(), savedFavorite.getBook().getTitle(),
                 user.getFullName()), HttpStatus.OK);
     }
 
     @GetMapping("/my-favorites")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
     public ResponseEntity<?> getUserInFavorite(){
         var email = servletRequest.getUserPrincipal().getName();
         var user = userRepo.findAppUserByEmail(email)
                 .orElseThrow(RuntimeException::new);
         log.info(servletRequest.getHeader(user.getEmail()));
 
-        var favList = favoriteService.getUser(user);
+        var favList = favoriteService.getUserFavorite(user);
+        if(favList.isEmpty()){
+            return new ResponseEntity<>("You don't have any favorite", HttpStatus.BAD_REQUEST);
+        }
+
         List<FavoriteResponseInJoin> joinList = new ArrayList<>();
         for(Favorite favorite: favList){
             joinList.add(FavoriteResponseInJoin.From(favorite));
         }
+        List<String> listOfBook = favList.stream()
+                .map(a -> a.getBook().getTitle())
+                .collect(Collectors.toList());
+        log.info("List of book: {}", listOfBook);
         return new ResponseEntity<>(joinList, HttpStatus.OK);
     }
 
--- src/main/java/com/auth/jwt/service/FavoriteService.java
+++ src/main/java/com/auth/jwt/service/FavoriteService.java
@@ -19,16 +19,17 @@ public class FavoriteService {
     private final FavoriteRepo favoriteRepo;
     private final BooksRepo booksRepo;
 
-    public Favorite createFavorite(Long bookId, AppUser user){
+    public Favorite createFavorite(Long bookId, String content, AppUser user){
         var book = booksRepo.findById(bookId)
                 .orElseThrow(RuntimeException::new);
         var favorite = new Favorite();
         favorite.setUser(user);
+        favorite.setContent(content);
         favorite.setBook(book);
         return favoriteRepo.save(favorite);
     }
 
-    public List<Favorite> getUser(AppUser user){
+    public List<Favorite> getUserFavorite(AppUser user){
         return favoriteRepo.findFavoriteByUser(user);
     }
 
