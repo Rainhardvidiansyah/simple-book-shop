be071ebba68a685982b227356f7c0d40c7595b8d
Rainhardvidiansyah <rainhardvidians@gmail.com>
2022-10-13 01:57:26 +0700

    make transaction response

---
+++ src/main/java/com/auth/jwt/dto/response/TransactionResponseDto.java
@@ -0,0 +1,34 @@
+package com.auth.jwt.dto.response;
+
+
+import com.auth.jwt.model.Transaction;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class TransactionResponseDto {
+
+    private Long transactionId;
+
+    private String paymentMethod;
+
+    private String orderNumber;
+
+    private double totalPrice;
+
+    private String senderAccountNumber;
+
+    private String senderBank;
+
+    public static TransactionResponseDto from(Transaction transaction){
+        return new TransactionResponseDto(
+                transaction.getId(), transaction.getPaymentMethod(),
+                transaction.getOrderNumber(), transaction.getTotalPrice(),
+                transaction.getSenderAccountNumber(), transaction.getSenderBank());
+    }
+
+
+}
